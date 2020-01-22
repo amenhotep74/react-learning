224a59d8251be296c81ccd0dc23eb93cb18f8be2
unknown <Blewie144@gmail.com>
2020-01-22 15:36:20 +1030

    Using props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import MyComponent from './MyComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: 'App title'
+    };
+    
+    this.onClick = this.onClick.bind(this);
+  }
+  
+  onClick(event) {
+    this.setState({
+      title: 'New App title'
+    });
+  }
+
+  onSubmit(event){
+    event.preventDefault();
+    alert(this.input.value);
+  }
+
+  onChange(event){
+    console.log(event.target.value);
+  }
+  render() { 
+    return ( 
+      <div>
+        <h1>{this.state.title}</h1>
+        <div onClick={this.onClick}>Click here</div>
+        <MyComponent 
+          title="This is the component title"
+          name="bradley"
+          onClick={this.onClick}
+        />
+      </div>
+    );
+  }
 }
-
+ 
 export default App;
---
+++ src/MyComponent.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class MyComponent extends Component {
+    state = {  }
+    render() { 
+
+        return ( 
+        <div>
+            <h1>Title: {this.props.title}</h1> 
+            <h2>Name: {this.props.name}</h2>
+            <div onClick={this.props.onClick}>Click me</div>
+        </div>
+        );
+    }
+}
+ 
+export default MyComponent;
